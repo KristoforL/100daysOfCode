4f876408be7a803a7786f5f720797d0961936eea
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-09-17 21:56:12 -0400

    Day 4 code of code started
    I have the code written but will be updated tomorrow with day 5 of code. Just a little exhausted from today.

---
+++ Day 4/Day 4 of code.py
@@ -0,0 +1,5 @@
+#Day 4 of Code
+
+### Blocks printed with hyphens between are seperate lessons and will have comments for what is covered in the space ###
+
+###--Continued List, for loops,  numbers in list, and using functions like --###
