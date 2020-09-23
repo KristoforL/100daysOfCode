1f97578ccbfabb6917b9913467508a9c4a04a669
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-09-22 20:02:02 -0400

    Day 7 Started

---
+++ Day 7/Day 7 of code.py
@@ -0,0 +1,7 @@
+#Day 6 of Code
+
+### Blocks printed with hyphens between are seperate lessons and will have comments for what is covered in the space ###
+
+###--Dictionaries, looping through them, key value pairs--###
+
+#Glossary: 
