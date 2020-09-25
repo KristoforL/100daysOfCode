a6b6fcb26a5c5c0539c521416e2ae48e66a2a37e
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-09-24 22:40:21 -0400

    Day 8 code started

---
+++ Day 8/Day 8 of code.py
@@ -0,0 +1,5 @@
+#Day 8 of Code
+
+### Blocks printed with hyphens between are seperate lessons and will have comments for what is covered in the space ###
+
+###--Users input, manipulating user input--###
