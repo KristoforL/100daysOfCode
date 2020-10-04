ca9f40c0adb6f4349311d064d3b7f35d85d82c74
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-10-03 20:51:58 -0400

    Day 13 code completed and uploaded

--- Day 13/Day 13 of code.py
+++ Day 13/Day 13 of code.py
@@ -6,3 +6,53 @@
 
 #Restaurant: Makae eclass. The __init__() method for class should store 2 attritbutes called describe_rest() that these two pieces of information and a mthod called open_rest() that prints these two pieces of information indicating the restaurant is open: Make an instance called restaurant from your class. Print two attributes individually and then call both method.
 
+class restaurant:
+    """Creates restaurant"""
+    def __init__(self, name, cuisine):
+        """Initializes attributes"""
+        self.name = name
+        self.cuisine = cuisine
+
+    def describe(self):
+        """Describes restaurant"""
+        print(f"{self.name} serves {self.cuisine} cuisine")
+
+    def open(self):
+        """Opens restaurant"""
+        print(f"{self.name} is open")
+
+thompson_bros = restaurant('Thompson Bros', 'BBQ')
+thompson_bros.describe()
+thompson_bros.open()
+
+#Three restaurants: Create 3 different restaurants and call describe() for each
+
+fishy =  restaurant('All Fish', 'seafood')
+smokey = restaurant('Smoke J','Smokey')
+southern = restaurant('Soul Shot','Southern')
+
+fishy.describe()
+smokey.describe()
+southern.describe()
+
+#Create two attribute user class. Firest and last name then create another attribute that would be associated with user information. Create describe user and greet user methods
+
+class user:
+    """Creates a user"""
+    def __init__(self, first, last, location):
+        self.first = first
+        self.last = last
+        self.location = location
+
+    def describe(self):
+        """Describes user"""
+        print(f"{self.first} {self.last} is from {self.location}")
+
+    def greet(self):
+        """Greets user"""
+        print(f"Welcome {self.first} {self.last}")
+
+me = user('J','L','ATL')
+me.describe()
+me.greet()
+
