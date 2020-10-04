698db44b455d813e3e89fd97e38738144f1ac1d1
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-10-03 20:53:23 -0400

    Day 14 started but not complete

---
+++ Day 14/Day 14 of code.py
@@ -0,0 +1,8 @@
+#Day 14 of Code
+
+### Blocks printed with hyphens between are seperate lessons and will have comments for what is covered in the space ###
+
+###--Classes Continued, changing values in functions, manipulating values with methods--###
+
+
+#Numbere served
