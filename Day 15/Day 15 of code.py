e378cc5809e594ccbb005c90b8ecc5d5622b504a
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-10-05 22:09:29 -0400

    Day 15 started before typing it all

--- Day 15/Day 15 of code.py
+++ Day 15/Day 15 of code.py
@@ -3,3 +3,5 @@
 ### Blocks printed with hyphens between are seperate lessons and will have comments for what is covered in the space ###
 
 ###--Classes Continued, changing values in functions, manipulating values with methods, importing from other files current files--###
+
+#Admin:
