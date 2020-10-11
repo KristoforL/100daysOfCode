2a8b876773f110662f04e622c364454891b1c6ef
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-10-10 22:21:04 -0400

    Started looking at the pmotw
    1. Strings. templates, and constants

---
+++ Python Module of the week/Strings Constants and templates.py
@@ -0,0 +1,19 @@
+#Python Module of the week wehere I will be looking into the strings constants and templates
+
+#Capwords: This function capitalizes the words in the string
+
+import string
+
+s = "i really like pie. i can't wait for thanksgiving."
+print(s)
+print(string.capwords(s))
+
+#You can get the same results from splitting the string capitalizing each word then joining them back together. 
+
+#
+
+value = {'foo':'var'}
+
+
+
+
