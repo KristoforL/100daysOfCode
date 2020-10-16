fe3f23ef88d19390f6d6723c06457ec9d043a43a
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-10-16 14:49:00 -0400

    Update Day 19 of code.py

--- Day 19/Day 19 of code.py
+++ Day 19/Day 19 of code.py
@@ -3,3 +3,5 @@
 ### Blocks printed with hyphens between are seperate lessons and will have comments for what is covered in the space ###
 
 ###--Files and exceptions. creating/editing/opening --###
+
+#Fav Number:
