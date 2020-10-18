28b45610717fecefddcb538bf7b25128cc8ff7cf
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-10-18 17:44:03 -0400

    This works now
    I am not sure why it was not working yesterday but it works now

--- Day 19/Day 19 of code.py
+++ Day 19/Day 19 of code.py
@@ -9,16 +9,16 @@
 
 import json
 
-# file = 'numbers.json'
+ file = 'numbers.json'
 
-# fav = input('What is your favorite number?\n')
+ fav = input('What is your favorite number?\n')
 
-# with open(file, 'w') as f:
-#     json.dump(fav, f)
+ with open(file, 'w') as f:
+     json.dump(fav, f)
 
-# with open(file) as f:
-#     number = json.load(f)
-# print(f'I know what your favorite number is. It is {number}')
+ with open(file) as f:
+     number = json.load(f)
+ print(f'I know what your favorite number is. It is {number}')
 
 
 #Fav number, remembered: Combine two blocks of code from avove into a file. If the number is already stored report the fav number to the user. If not, prompt for the users fav number and store it in a file. Run 2x to see if it works
