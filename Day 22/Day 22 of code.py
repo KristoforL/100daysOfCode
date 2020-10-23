8b37390035d0abcffe1a2bcb0c0e5f3a89bc0bc4
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-10-22 23:31:15 -0400

    Bitwise operator practice

---
+++ Day 22/Day 22 of code.py
@@ -0,0 +1,29 @@
+#Day 22 of Code
+
+### Practice in understanding bitwise operators ###
+
+
+# & Bitwise Operator
+
+print(11 & 12)
+
+# | Bitwise operator
+
+print(11 | 12)
+
+
+print(11<<2)
+
+
+print(11>>2)
+
+
+print(11^12)
+
+
+print(~11)
+
+
+print(~57)
+
+print(~172)
