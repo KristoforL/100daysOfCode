e7d5c86115a02c1850e6901315a254939543ca9f
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-03 00:22:39 -0500

    Updating Bitwise operators and practicing as well

--- Day 22/Day 22 of code.py
+++ Day 22/Day 22 of code.py
@@ -4,12 +4,17 @@
 
 
 # & Bitwise Operator
-
+# The & bitwise operator will combine the bits for each number and return 1 if both are 1 at the same position
 print(11 & 12)
+#1011 & 1100 = 1000
+#1000 is the bit for 8
+
 
 # | Bitwise operator
-
+# The | operator will combine the bits for the numbers and place a 1 if either of the two bits being compared are 1
 print(11 | 12)
+#1011 & 1100 = 1111
+#1111 is the bit for 15
 
 
 print(11<<2)
@@ -26,4 +31,6 @@ print(~11)
 
 print(~57)
 
-print(~172)
+print(~14)
+
+print(~333)
