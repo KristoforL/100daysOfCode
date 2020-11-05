cc840585b9d3c85cd10489321d8c6e32bffac538
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-05 02:56:37 -0500

    functions
    function import randoms sorts

--- Day 24/Day 24 of code.py
+++ Day 24/Day 24 of code.py
@@ -14,3 +14,57 @@
 # Randomly generate two lists to test this
 # Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 # List properties
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+def sameElements(a, b):
+    """Takes in two list and return a list of the elements that are the same"""
+    #Creates a list that will store all the same values from each list
+    c = []
+    #Takes one element at a time and compares it to other list to see if it is in the list and then makes sure it is not already in the new c before appending it in the list so that there are no copies in list c
+    for element in a:
+        if element in b and element not in c:
+            c.append(element)
+    #Prints all the items that are in both list
+    print(f"All elements that are in both list\n{c}")
+
+
+sameElements(a,b)
+
+###---------------Extra-----------###
+
+import random
+
+def randomListCompare():
+    """Generates two randome element list and return a list of the elements that are the same"""
+
+    a = []
+    b = []
+
+    length = random.randint(1,101)
+    for number in range(length):
+        value = random.randint(0,101)
+        a.append(value)
+        
+    length = random.randint(1,101)
+    for number in range(length):
+        b.append(random.randint(0,101))
+    
+    #Creates a list that will store all the same values from each list
+    c = []
+    for element in a:
+        if element in b and element not in c:
+            c.append(element)
+
+    #Takes one element at a time and compares it to other list to see if it is in the list and then makes sure it is not already in the new c before appending it in the list so that there are no copies in list c
+    for element in a:
+        if element in b and element not in c:
+            c.append(element)
+    #Prints all the items that are in both list
+    
+    print(f"The list generated are a:\n{a}\nb:\n{b}\n")
+    c.sort()
+    print(f"All elements that are in both list\n{c}")
+
+randomListCompare()
