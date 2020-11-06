016245bca392d9d6735136abb88d35c1b8d042d0
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-06 13:50:03 -0500

    Day 26 work started

---
+++ Day 26/Day 26 of code.py
@@ -0,0 +1,14 @@
+#This is further practive bfore the test on Sunday. I will be reading the chapters again from the pcep website and having some of the practice executed here
+
+###--Practice and review questions before PCEP--###
+#Same as from the 21st day of code practice
+# I have gotten some of these practice problems from https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
+
+# Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
+
+# Remember the rules:
+
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
