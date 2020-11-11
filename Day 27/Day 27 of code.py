364dc491470f031ef9fd14319fd7ef141a4deef7
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-11 12:40:57 -0500

    Create Day 27 of code.py
    Starting day 27

---
+++ Day 27/Day 27 of code.py
@@ -0,0 +1,12 @@
+#This is further practive bfore the test on Sunday. I will be reading the chapters again from the pcep website and having some of the practice executed here
+
+###--Practice and review questions before PCEP--###
+#Same as from the 21st day of code practice
+# I have gotten some of these practice problems from https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
+# Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right. (Hint: remember to use the user input lessons from the very first exercise)
+
+# Extras:
+
+# Keep the game going until the user types “exit”
+# Keep track of how many guesses the user has taken, and when the game ends, print this out.
