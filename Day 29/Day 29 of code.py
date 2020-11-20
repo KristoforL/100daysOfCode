5cd4389e08f967776faea396dd010b466c7550ee
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-20 12:55:03 -0500

    Update Day 29 of code.py
    Practicing more list comprehension and finding elements that are the same in both list with no duplication.

--- Day 29/Day 29 of code.py
+++ Day 29/Day 29 of code.py
@@ -17,3 +17,16 @@
 # Extra:
 
 # Randomly generate two lists to test this
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+common = []
+
+for item in a:
+    if item in b and item not in common:
+        common.append(item)
+
+
+print(f"The common items in the list are:\n{[number for number in common]}")
