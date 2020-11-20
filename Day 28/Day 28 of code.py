ede1b03a8cb99d9c9cf1a0e9ebdcc00db7a12d3b
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-20 12:42:43 -0500

    Update Day 28 of code.py
    Printing with efficiency. One line to handle multiple list comprehension.

--- Day 28/Day 28 of code.py
+++ Day 28/Day 28 of code.py
@@ -6,3 +6,7 @@
 
 #Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
 
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+print(f"Original list is {a}, and the even numbers are:\n{[item for item in a if item%2 ==0]}")
