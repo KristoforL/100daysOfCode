1105fde5148213984e26c07b5a5256b7a9037d13
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-24 13:33:26 -0500

    Update Day 33 of code.py
    Function works to go through the fib number sequence

--- Day 33/Day 33 of code.py
+++ Day 33/Day 33 of code.py
@@ -6,3 +6,27 @@
 
 
 #Write a program that asks the user how many Fibonnaci numbers to generate and then generates them. Take this opportunity to think about how you can use functions. Make sure to ask the user to enter the number of numbers in the sequence to generate.(Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+
+#Set global the liat to append to.
+fibList = []
+
+sets the function to the 
+def fib():
+    """Generates list of fibonnaci numbers the length of the parameter given"""
+    number = int(input("How many Fibonnaci numbers do you want to see:\n"))
+    count = 1
+    
+    if number == 0:
+        fibList = []
+    elif number == 1:
+        fibList == [1]
+    elif number == 2:
+        fibList == [1,1]
+    elif number > 2:
+        fibList = [1,1]
+        while count < (number-1):
+            fibList.append(fibList[count] + fibList[count-1])
+            count += 1
+    return fibList
+
+print(fib())
