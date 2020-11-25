f9c87edd5f5ac36e1bc7fff05706c5e36d57e3e6
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-25 16:01:53 -0500

    Will try to finish this chapter before Friday Night
    Relative and absolute path is broken in vscode until furhter notice

---
+++ Day 30/alien_invasion/alien_invasion.py
@@ -0,0 +1,45 @@
+import sys
+import pygame as pg
+
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Overall class to handle game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game and create resources."""
+        pg.init()
+        self.settings = Settings()
+        self.screen = pg.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pg.display.set_caption("Alien Invasion")
+        self.ship = Ship(self) 
+
+    #The below is placed in the settings.py so that it can be better managed
+        #self.screen = pg.display.set_mode((1200, 800))
+        #Set background color
+        #self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            #Watch for keyboard and mouse events
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    sys.exit()
+
+            #Redraw the screen during each pass through
+            self.screen.fill(self.settings.bg_color)
+
+            self.ship.blitme()
+
+            #Make the most recently drawn screen visible.
+            pg.display.flip()
+    
+if __name__ == '__main__':
+    #Mkae a game instace, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
+    
+
