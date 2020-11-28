80224ea33bd12d21c592baa9636b7eb9520561ec
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-27 19:01:32 -0500

    Practice problems fixed
    Need to resize the ship in Day 41 practice

--- Day 41/myGame.py
+++ Day 41/myGame.py
@@ -1,6 +1,8 @@
 import sys
 import pygame as pg
 
+from sprite import Spaceship
+
 class MyGame:
     """My first practice at making the pygame myself"""
 
@@ -10,19 +12,36 @@ class MyGame:
         self.screen = pg.display.set_mode((1200, 800))
         pg.display.set_caption('My Game')
         #Set background color
-        self.bg_color = (0, 0, 230)
+        self.bg_color = (150, 230, 230)
+        self.sprite = Spaceship(self)
 
     def run_game(self):
         while True:
-            #Watch for keyboard and mouse events
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    sys.exit()
+            self._update_screen()
+            self._check_events()
+        
+
+    
+    def _check_events(self):
+        #Watch for keyboard and mouse events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
 
             #Redraw the screen during each pass through
             self.screen.fill(self.bg_color)
             #Make the most recently drawn screen visible.
             pg.display.flip()
+
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        #Redraw the screen during each pass through
+        self.screen.fill(self.bg_color)
+        self.sprite.blitme()
+        #Make the most recently drawn screen visible.
+        pg.display.flip()
+
 
 if __name__ == '__main__':
     #Make a game instace, and run the game
