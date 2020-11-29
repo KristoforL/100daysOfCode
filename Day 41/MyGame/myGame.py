e2c090a7ab992d313ec6822e6b02854c6a2f9b6d
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-29 17:43:45 -0500

    Practice in pygame
    I implemented something in my study code and moved it to my practice code instead.

--- Day 41/MyGame/myGame.py
+++ Day 41/MyGame/myGame.py
@@ -2,25 +2,25 @@ import sys
 import pygame as pg
 
 from settings import Settings
-from ship import Ship
+from sprite import Spaceship
 
-class AlienInvasion:
-    """Overall class to handle game assets and behavior"""
+class MyGame:
+    """My first practice at making the pygame myself"""
 
     def __init__(self):
-        """Initialize the game and create resources."""
+        """First game window generation"""
         pg.init()
         self.settings = Settings()
         self.screen = pg.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
-        pg.display.set_caption("Alien Invasion")
-        self.ship = Ship(self) 
+        pg.display.set_caption("My Game")
+        self.sprite = Spaceship(self) 
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
-            self.ship.update()
+            self.sprite.update()
             self._update_screen()
 
     def _check_events(self):
@@ -37,31 +37,38 @@ class AlienInvasion:
     def _check_keydown_events(self, event):
         #Responds to keypresses
         if event.key == pg.K_RIGHT:
-            self.ship.moving_right = True
+            self.sprite.moving_right = True
         elif event.key == pg.K_LEFT:
-            self.ship.moving_left = True
+            self.sprite.moving_left = True
+
+        elif event.key == pg.K_UP:
+            self.sprite.moving_up = True
+        elif event.key == pg.K_DOWN:
+            self.sprite.moving_down = True
         
     def _check_keyup_events(self, event):
         #Responds to Key release
         if event.key == pg.K_RIGHT:
-            self.ship.moving_right = False
+            self.sprite.moving_right = False
         elif event.key == pg.K_LEFT:
-            self.ship.moving_left = False
+            self.sprite.moving_left = False
+
+        elif event.key == pg.K_UP:
+            self.sprite.moving_up = False
+        elif event.key == pg.K_DOWN:
+            self.sprite.moving_down = False
 
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         #Redraw the screen during each pass through
         self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
+        self.sprite.blitme()
         #Make the most recently drawn screen visible.
         pg.display.flip()
-        
 
-    
+
 if __name__ == '__main__':
     #Make a game instace, and run the game
-    ai = AlienInvasion()
+    ai = MyGame()
     ai.run_game()
-    
-
--- Day 41/MyGame/sprite.py
+++ Day 41/MyGame/sprite.py
@@ -1,6 +1,6 @@
 import pygame as pg
 
-class Ship:
+class Spaceship:
     """A class to manage the ship"""
 
     def __init__(self, ai_game):
@@ -9,11 +9,8 @@ class Ship:
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-
         #Load the ship image and get its rect.
-        #self.image = pg.image.load('Day 30\images\ship.bmp')
-        #For mac users
-        self.image = pg.image.load('Day 30/images/ship.bmp')
+        self.image = pg.image.load('Day 41/images/spaceship.bmp')
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen
@@ -21,24 +18,33 @@ class Ship:
 
         #Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         #Movement flag
         self.moving_right = False
         self.moving_left = False
 
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update the ships position based on movement flag"""
         #Update the ships x value, not the rect
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.ship_speed
+            self.x += self.settings.sprite_speed
         if self.moving_left and self.rect.left > 0:
-            self.x -= self.settings.ship_speed
+            self.x -= self.settings.sprite_speed
+
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.sprite_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.sprite_speed
         
         #update rect object from self.x
         self.rect.x =self.x
+        self.rect.y =self.y
+
 
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
-
