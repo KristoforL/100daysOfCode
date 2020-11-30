cad774b3a85cef7d3d79fe097d42b6c0350635bb
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-11-30 00:50:53 -0500

    Ship and backgroud done

--- Day 30/alien_invasion/alien_invasion.py
+++ Day 30/alien_invasion/alien_invasion.py
@@ -25,15 +25,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
-
-            #Get rid of bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
-            
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -67,8 +60,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Creates new bullet and adds it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position f bullets and get rid of old bullets"""
+        #Update bullet positions
+        self.bullets.update()
+        #Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
