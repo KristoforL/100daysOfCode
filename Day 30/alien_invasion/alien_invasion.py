b4c1518718518410850e6a56a177e835a96fd8ef
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-01 16:30:43 -0500

    Typos
    must keep checking typos and adding and ending to the game

--- Day 30/alien_invasion/alien_invasion.py
+++ Day 30/alien_invasion/alien_invasion.py
@@ -1,7 +1,10 @@
 import sys
+from time import sleep
+
 import pygame as pg
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -18,6 +21,9 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pg.display.set_caption("Alien Invasion")
+
+        #Create and instance to store game stats
+        self.stats=GameStats(self)
         self.ship = Ship(self) 
         self.bullets = pg.sprite.Group()
         self.aliens = pg.sprite.Group()
@@ -101,7 +107,24 @@ class AlienInvasion:
         self.aliens.update()
         #Look for alien-ship collisions
         if pg.sprite.spritecollideany(self.ship, self.aliens):
-            print("Ship Hit!!")
+            self._ship_hit()
+
+    
+    def _ship_hit(self):
+        """Responds to the ship being hit by an alien"""
+        #Decrement ships_left
+        self.stats.ships_left -= 1
+
+        #Get rid of any remianing aliens and bullets
+        self.aliens.empty()
+        self.bullets.empty
+
+        #Create a new fleet and center the ship
+        self._create_fleet()
+        self.ship.center_ship()
+
+        #Pause
+        sleep(0.5)
 
 
     def _create_fleet(self):
