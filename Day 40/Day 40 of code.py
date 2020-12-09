06163c7a79b9b32f994c1ef04fa5f87f232f9565
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-08 23:22:20 -0500

    Updated the folders for Data visualization
    Checking all my folders before getting them ready for the data vsiualization studying

--- Day 40/Day 40 of code.py
+++ Day 40/Day 40 of code.py
@@ -1,11 +1,1 @@
-#This is further practive bfore the test on Sunday. I will be reading the chapters again from the pcep website and having some of the practice executed here
-
-###--Practice and review questions before PCEP--###
-#Same as from the 21st day of code practice
-# I have gotten some of these practice problems from https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
-
-# Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) and another number. The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
-
-# Extras:
-
-# Use binary search.
+#Data visualization in the no scratch python book
