34e9fb4a6860a50ea083df42355564f39f62e5a0
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-08 23:31:59 -0500

    I cannot run this on my windows machine
    Just trying to run some programs on my pc and it seems matplotlib does not like pc so I will look into this but continue to code and try this on my macbook

--- Day 37/Day 37 of code.py
+++ Day 37/Day 37 of code.py
@@ -1,1 +1,22 @@
 #Data visualization in the no scratch python book
+
+import matplotlib.pyplot as plt
+
+
+squares = [1,4,9,16,25]
+
+fig, ax = plt.subplots()
+ax.plot(squares)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
