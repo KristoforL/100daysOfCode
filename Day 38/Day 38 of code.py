8072d3208d0b2bb9c7dc2b18db67ce86022cbc71
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-09 15:44:55 -0500

    Random walk plots

--- Day 38/Day 38 of code.py
+++ Day 38/Day 38 of code.py
@@ -1,1 +1,19 @@
 #Data visualization in the no scratch python book
+
+
+from random import choice
+
+class Randomwalk():
+    """A class to genrate random walks"""
+
+    def __init__(self, num_points=5000):
+        """Initiliaze attributes of a walk"""
+        self.num_points = num_points
+
+        #All walks start at (0,0)
+        self.x_values = [0]
+        self.y_values = [0]
+
+
+    def fill_walk(self):
+        """Calculate all the points in the walk
