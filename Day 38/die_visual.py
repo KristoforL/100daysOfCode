73f5aadd00cfd14c5a284032a9d77054c3244742
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-10 22:40:36 -0500

    Finished the chapter
    Finished the beginning of intro to data visualization. Next day we will be working on the utilizing real world data.

--- Day 38/die_visual.py
+++ Day 38/die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 
@@ -11,9 +14,18 @@ for roll_num in range(1000):
     results.append(result)
 
 #Analyze the results
-frequncies = []
+frequencies = []
 for value in range(1, die.num_sides+1):
     frequncy = results.count(value)
-    frequncies.append(frequncy)
+    frequencies.append(frequncy)
 
-print(frequncies)
+#Visulaize the results
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y= frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title = 'Results of rolling one D6 1000 times', xaxis = x_axis_config, yaxis=y_axis_config)
+offline.plot({'data':data, 'layout':my_layout}, filename='Day 38/d6.html')
+
+print(frequencies)
