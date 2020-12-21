c46303fbe803d37e2201ec0078d60dcd2db9f8a4
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-21 13:51:11 -0500

    Udemy 100 days fo code start

---
+++ Day 1/Udemy Day 1.py
@@ -0,0 +1,15 @@
+print('Hello World')    
+
+#Day 1 asked me to print the following: 
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+print('Day 1 - Python Print Function')
+print('What to print?')
+print("print('what to print')")
+
+#Here we will learn about manipulating the strings
+#New linnes use \n which means new line
+#We can add strings together as well with concatination
+print('This is all on the same line\nin the code')
+print('Hello ' + 'Kris')
