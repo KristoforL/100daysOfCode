3cf25d3572a8278ba6225e25fdb544bc805a9828
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-21 14:06:15 -0500

    Challenges for Udemy Day 1.
    1 hour of work for  the day maybe more so that I can ramp up pass the first few days

--- Day 1/Udemy Day 1.py
+++ Day 1/Udemy Day 1.py
@@ -1,6 +1,6 @@
 print('Hello World')    
 
-#Day 1 asked me to print the following: 
+#Day 1 Challenge 1 asked me to print the following: 
 # Day 1 - Python Print Function
 # The function is declared like this:
 # print('what to print')
@@ -9,7 +9,18 @@ print('What to print?')
 print("print('what to print')")
 
 #Here we will learn about manipulating the strings
-#New linnes use \n which means new line
+#New lines use \n which means new line
 #We can add strings together as well with concatination
 print('This is all on the same line\nin the code')
 print('Hello ' + 'Kris')
+
+#Day 1 challenge 2 asked me to correct the following code:
+# print(Day 1 - String Manipulation")- missing quotation
+# print("String Concatenation is done with the "+" sign.") - The plus sign is part of the string and not adding the strings together
+#   print('e.g. print("Hello " + "world")')- Indenting problems
+# print(("New lines can be created with a backslash and n.") - extra parenthesis
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
