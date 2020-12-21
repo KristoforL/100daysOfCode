5ee8f5177d02fa00334517d48bb56f0b92c1c1ea
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-21 14:58:14 -0500

    Completed Challenges
    Doing challenges for the Udemy 100 days of code.

--- Day 1/Udemy Day 1.py
+++ Day 1/Udemy Day 1.py
@@ -24,3 +24,37 @@ print("Day 1 - String Manipulation")
 print("String Concatenation is done with the '+' sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+#Input function is used so that you can take input from a user. The information is saved and input is now a variable
+#You can even print it all in one line so that you can address yourself
+input(('What is your name: ')) #Do not put print into the input function as it will print the statement for you
+print('Hello '+ input('What is your name: '))
+name = input("What is your name:\n")
+
+#Day 1 Challenge 2 asked me to print out the length of a persons name
+#Because I am far more advanced I know how to use the len() function
+#Below I have brokoen each function with indention to show what is happening
+print(
+    len(
+        input(
+            'What is your name: ')))
+
+
+#Day 1 Challenge 3 asked me to switch variable values with the code they have provided
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+
+c = a #Pretty simple to add a variable that is not equal to anything
+a = b #Set a equal to be and b = c 
+b = c
+
+#Write your code above this line 👆
+####################################
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
