7d70a0d839bdbecc4f1237426e5e3f24dffa0b1d
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-27 13:30:05 -0600

    Clean code
    Efficient clean code now

--- Day 4/Rock_Paper_Scissors.py
+++ Day 4/Rock_Paper_Scissors.py
@@ -5,126 +5,70 @@ player = input('Choose R for Rock, P for Paper, S for Scissors.\n')
 
 computer = random.randint(1,3)
 
-#Rock
-if player.lower() == 'r':
-    print("""
+rock = """
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-""")
-    if computer == 1:
-        print("""
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-""")
-        print('Computer threw rock. It is a tie')
-    elif computer == 2:
-        print("""
+"""
+paper = """
      _______
 ---'    ____)____
            ______)
           _______)
          _______)
 ---.__________)
-""")   
-        print('Copmuter threw paper. Computer wins')
-    elif computer == 3:
-        print("""
+"""
+scissors = """
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-""")
+"""
+
+RPS = [rock, paper, scissors]
+
+
+#Rock
+if player.lower() == 'r':
+    print(RPS[0])
+    if computer == 1:
+        print(RPS[0])
+        print('Computer threw rock. It is a tie')
+    elif computer == 2:
+        print(RPS[1])   
+        print('Copmuter threw paper. Computer wins')
+    elif computer == 3:
+        print(RPS[2])
         print('Computer threw scissors. You win')
 
 #Paper
 elif player.lower() == 'p':
-    print("""
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-""")
+    print(RPS[1])
     if computer == 1:
-        print("""
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-""")
+        print(RPS[0])
         print('Computer threw rock. You Win')
     elif computer == 2:
-        print("""
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-""")   
+        print(RPS[1])   
         print('Copmuter threw paper. It is a tie')
     elif computer == 3:
-        print("""
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-""")
+        print(RPS[2])
         print('Copmuter threw scissors. You Lose')
 #Scissors
 elif player.lower() == 's':
-    print("""
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-""")
+    print(RPS[2])
     if computer == 1:
-        print("""
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-""")
+        print(RPS[0])
         print('Computer threw rock. You lose')
     elif computer == 2:
-        print("""
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-""")   
+        print(RPS[1])   
         print('Copmuter threw paper. You Win')
     elif computer == 3:
-        print("""
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-""")
+        print(RPS[2])
         print('Compuer threw scissors. It is a tie.')
 
 else:
