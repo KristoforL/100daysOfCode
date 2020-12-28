3c146948f600668a4759897a3e409788f1483650
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-27 21:58:19 -0600

    Challenges completed for Day 6

---
+++ Day 6/Udemy Day 6.py
@@ -0,0 +1,47 @@
+#This is for Udemy focusing on Beginners Python functions and Karel the robot
+#More loops and calling functions
+
+#Functions are recognized by name and immediate parenthesis
+#Methods usually follow a variable with a <.'name of method'()>
+#Print() is a funcitons
+
+#We can create our own functions by defining def with a name parenthesis and then a colon
+def myFunction():
+    print('Hello')
+    print('Bye')
+
+#You must call your function for it to be triggered.
+myFunction()
+
+#Indentation determines blocks of codes functions and loop items
+    
+#While loop are loops that will have a condition that needs to be met before it stops. Copmared to for loops, for loops will continue for a set amount of time specified. While loops might not ever end
+
+#For loops are good for iterating and performing task with each iteration
+#While loops are better when there is not clear indication of how many iterations are needed to meet the goal
+
+
+#There were about 4 or 5 Challenges and all dealing with loops found on this page: https://www.udemy.com/course/100-days-of-code/learn/lecture/19115618#overview
+
+#Code below for the final loops
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()         
+    
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+#         jump()
