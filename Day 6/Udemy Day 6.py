d1511037888d45917a9396685c1fa96cc2f1b576
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-12-27 22:37:39 -0600

    Finished the reborg final project

--- Day 6/Udemy Day 6.py
+++ Day 6/Udemy Day 6.py
@@ -45,3 +45,22 @@ myFunction()
 #     else:
 #         turn_left()
 #         jump()
+
+
+
+
+#The final project is the reborgs world maze where we get reborg to leave the maze
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()       
+    
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
