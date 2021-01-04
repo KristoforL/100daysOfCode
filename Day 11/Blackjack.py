3d344dad8b2b89eea62875548b969fcfe1233fda
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2021-01-04 12:12:12 -0500

    looking at the hints

--- Day 11/Blackjack.py
+++ Day 11/Blackjack.py
@@ -1,1 +1,13 @@
 #Official blackjack
+import random as r
+import blackjack_art as ba
+import math as m
+
+print(f'{ba.logo}')
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+player = []
+computer = []
+
+
+
