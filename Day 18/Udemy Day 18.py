75f6fa8ca71da9fb5316ba9797c327cc412af60c
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2021-01-11 12:24:40 -0500

    Setting change

--- Day 18/Udemy Day 18.py
+++ Day 18/Udemy Day 18.py
@@ -1,1 +1,25 @@
 #Todays lesson will be interacting with the GUI, Tuples, and finishing up with making art
+import turtle as t
+
+timmy = t.Turtle()
+screen = t.Screen()
+screen.exitonclick()
+
+print(timmy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
